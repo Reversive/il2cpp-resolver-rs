4b79c9a880110f47c91c518982a1e7f79fcc50f0
Reversive <matiaspavan@gmail.com>
2023-04-28 23:27:34 -0300

    feat: process address export resolver

--- src/il2cpp/mod.rs
+++ src/il2cpp/mod.rs
@@ -11,7 +11,6 @@ pub fn initialize(wait_for_module : bool) -> bool {
     use defines::IL2CPP_MAIN_MODULE;
     let ctx = &mut Context::new();
     loop {
-        
         if let Ok(module) = unsafe { GetModuleHandleA(PCSTR::from_raw(IL2CPP_MAIN_MODULE.as_ptr())) } {
             ctx.game_assembly_module = module.0;
             break;
@@ -23,7 +22,7 @@ pub fn initialize(wait_for_module : bool) -> bool {
 
     if ctx.game_assembly_module == 0 { return false; }
 
-    if !unity_api::initialize() { return false; }
+    if !unity_api::initialize(ctx) { return false; }
 
     true
 }
